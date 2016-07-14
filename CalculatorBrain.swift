fd4a264ee0f1fc4572133421762cc9db4108211d
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-07-14 17:44:14 +0100

    Create Calculator Brain class which is the Application model and logic

---
+++ CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  IOSCalculator
+//
+//  Created by spykins on 14/07/2016.
+//  Copyright © 2016 Andela. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+}
