53fe3a28cb79bdc177766fc0ac7f57577d22723f
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-07-15 20:34:41 +0100

    Implementing the constant value, but instead of placing all of them in a switch statement, we will place them in a table , dictionary, which will allow us get the value based on the key

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -9,13 +9,24 @@
 import Foundation
 
 class CalculatorBrain {
-    func setOperand (operand : Double) {}
-    func performOperation (symbol : String) {}
+    private var accumulator = 0.0
+    func setOperand (operand : Double) {
+        accumulator = operand
+    }
+    
+    func performOperation (symbol : String) {
+        switch symbol {
+        case "π" : accumulator = M_PI
+        case "√" : accumulator = sqrt(accumulator)
+        default:
+            break
+        }
+    }
     
     //Placing get inside the result, makes it read only.. So client can't set it
     var result : Double {
         get{
-           return 0.0
+            return 0.0
         }
     }
 }
