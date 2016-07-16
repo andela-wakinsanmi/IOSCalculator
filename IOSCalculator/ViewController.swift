d2c195a4c7fcd0db6d9d03bd9e8ffa34e17cb9a1
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-07-16 18:41:38 +0100

    Wire up the UI using stack view

--- IOSCalculator/ViewController.swift
+++ IOSCalculator/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //×
+    /*
+        Our UI.... What we need to do is to fix the UI so that when it's in portrait, It is using the whole available space.. When in landscape, the button covers the whole screen
+     
+     To do that,  We will take each of the button row eg (π789) and put them in a little stack
+     THen take the whole column of 5 stacks and stack them together..
+     
+     Then we will stack the whole thing with Textview ...And create stack of stack
+     
+     Then we will bind the left top right and bottom edges of the whole thing to the outer edges of the UI
+     
+     That way when the outer edges of the UI changes, that thing will change and the stack will realocate the space
+     
+     To Make stack
+     1. Select things you want to stack
+     2. Go to Editor >> Embed In > Stack View
+     
+     Now to change the attricute, Select the stack view, go to attribute inspector
+     change spacing to 10
+     Distributed space equally
+     
+     After stacking the whole view
+     Place it in the highlight left corner, Now, anchor it
+     press the control key, drag from the stack container to the outer container by adding constrain
+     
+     To know whaht you are selecting
+     Control Shift click
+     
+    */
 
     @IBOutlet private weak var display: UILabel!
     private var userIsInTheMiddleOfTyping = false
